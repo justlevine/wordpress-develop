38483caf01693a29e1a55ef0b69bda4ca6bb91ea
Arnt Gulbrandsen <arnt@gulbrandsen.priv.no>
2024-09-23 14:20:09 +0200

    General: Add support for most unicode letters in WP backend login names
    This accepts user names that contain a single script, but not mixed-script
    names, such as ones that mix Latin and Cyrillic. That seemed to be closest
    to the code's existing philosophy.
    
    Since PHP 7.2 and 7.3 don't offer mb_str_split, this change leaves sites
    on those versions of PHP with ASCII-only user names.
     - Existing user names continue to work unchanged even on 7.2/7.3
     - adding new non-ASCII user names requires 7.4
     - autocorrection requires 7.4 (e.g. user'@example.com -> user@example.com)

--- tests/phpunit/tests/formatting/sanitizeUser.php
+++ tests/phpunit/tests/formatting/sanitizeUser.php
@@ -24,6 +24,9 @@ class Tests_Formatting_SanitizeUser extends WP_UnitTestCase {
 	}
 
 	public function test_strips_encoded_ampersand_when_followed_by_semicolon() {
+		if ( ! function_exists( 'mb_str_split' ) ) {
+			$this->markTestSkipped( 'PHP 7.2/3 lacks mb_str_split' );
+		}
 		$expected = 'ATT Test;';
 
 		// Multisite forces user logins to lowercase.
@@ -34,11 +37,53 @@ class Tests_Formatting_SanitizeUser extends WP_UnitTestCase {
 		$this->assertSame( $expected, sanitize_user( 'AT&amp;T Test;' ) );
 	}
 
+	/*
+	 * @ticket 31992
+	 */
+
 	public function test_strips_percent_encoded_octets() {
-		$expected = is_multisite() ? 'franois' : 'Franois';
+		if ( ! function_exists( 'mb_str_split' ) ) {
+			$this->markTestSkipped( 'PHP 7.2/3 lacks mb_str_split' );
+		}
+		$expected = is_multisite() ? 'françois' : 'François';
 		$this->assertSame( $expected, sanitize_user( 'Fran%c3%a7ois' ) );
 	}
+
 	public function test_optional_strict_mode_reduces_to_safe_ascii_subset() {
 		$this->assertSame( 'abc', sanitize_user( '()~ab~ˆcˆ!', true ) );
+	}
+
+	public function test_accepts_all_arabic() {
+		if ( ! function_exists( 'mb_str_split' ) ) {
+			$this->markTestSkipped( 'PHP 7.2/3 lacks mb_str_split' );
+		}
+		$expected = 'آرنت';
+		$encoded  = '%D8%A2%D8%B1%D9%86%D8%AA';
+
+		$this->assertSame( $expected, sanitize_user( $expected ) );
+		$this->assertSame( $expected, sanitize_user( $encoded ) );
+	}
+
+	/*
+	 * @ticket 31992
+	 */
+
+	public function test_accepts_west_african_latin() {
+		if ( ! function_exists( 'mb_str_split' ) ) {
+			$this->markTestSkipped( 'PHP 7.2/3 lacks mb_str_split' );
+		}
+		$expected = 'tɔnatɔn';
+		$encoded  = 't%C9%94nat%C9%94n';
+
+		$this->assertSame( $expected, sanitize_user( $expected ) );
+		$this->assertSame( $expected, sanitize_user( $encoded ) );
+	}
+
+	/*
+	 * @ticket 31992
+	 */
+
+	public function test_blocks_latin_cyrillic_mixed_name() {
+		$this->assertSame( 'arn', sanitize_user( 'arn%D1%82' ) );
 	}
 }
--- tests/phpunit/tests/user.php
+++ tests/phpunit/tests/user.php
@@ -849,6 +849,38 @@ class Tests_User extends WP_UnitTestCase {
 	}
 
 	/**
+	 * Check that single-script usernames work, mixed-script ones
+	 * don't, and really strange ones don't.
+	 *
+	 * Since validate_username() uses strict sanitization, this
+	 * test checks some codepoints strictly, rejecting some that
+	 * are less readable than confusable.
+	 */
+	public function test_validate_utf8_usernames() {
+		if ( ! function_exists( 'mb_str_split' ) ) {
+			$this->markTestSkipped( 'PHP 7.2/3 lacks mb_str_split' );
+		}
+		/* WordPress approves of drab grey (grå) Norwegian weather */
+		$this->assertTrue( validate_username( 'grå' ) );
+		/* Latin I, Cyrillic V like latin B, Latin M */
+		$this->assertFalse( validate_username( 'IВM' ) );
+		/* Three Cyrillic letters */
+		$this->assertTrue( validate_username( 'ІВМ' ) );
+		/* A metal umlaut fails because validate_username is
+		 * strict and n̈ is unfamiliar in every language
+		 */
+		$this->assertFalse( validate_username( 'spın̈altap' ) );
+		/* Emoji skintones fail because usernames should be
+		 * easily distinguishable
+		 */
+		$this->assertFalse( validate_username( '👱🏼' ) );
+		$this->assertFalse( validate_username( '👱🏾' ) );
+		/* Many plain emoji also are barely distinguishable */
+		$this->assertFalse( validate_username( '😊' ) );
+		$this->assertFalse( validate_username( '☺️' ) );
+	}
+
+	/**
 	 * @ticket 29880
 	 */
 	public function test_wp_insert_user_should_not_wipe_existing_password() {
