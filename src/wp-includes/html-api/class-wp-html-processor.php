e51a575a174e5fa8bc77811af79554dce0d691fe
Dovid Levine <david@axepress.dev>
2025-10-25 12:12:48 +0300

    chore(phpstan): add phpstan level 0 annotations to code

--- src/wp-includes/html-api/class-wp-html-processor.php
+++ src/wp-includes/html-api/class-wp-html-processor.php
@@ -139,6 +139,8 @@
  *
  * @see WP_HTML_Tag_Processor
  * @see https://html.spec.whatwg.org/
+ *
+ * @phpstan-consistent-constructor
  */
 class WP_HTML_Processor extends WP_HTML_Tag_Processor {
 	/**
